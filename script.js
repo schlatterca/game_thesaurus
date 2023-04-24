e33f01b51582ca167da8d94e0e1043e39438300b
Carlo <48732275+schlatterca@users.noreply.github.com>
2023-04-24 16:17:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-(async() => {
-	//const URL = "text_dumps";
-	const URL = 'https://github.com/schlatterca/vg_text/tree/main/text_dumps';
-	//const doc = await fetch(URL+"?filter[metadata.mimeType]=application%2Fvnd.google-apps.spreadsheet").then(r => r.json());
-	const doc = await fetch(URL).then(r => r.json());
-	console.log(URL, doc);
-	console.log('1');
+jQuery.get('list.csv', function(data) {
+    //console.log(data);
+
+    var rowData = data.split('\n');
+
+    // <table > <tbody>
+    var tbodyEl = document.getElementById('tblcsvdata').getElementsByTagName('tbody')[0];
+    tbodyEl.innerHTML = "";
+
+    // Loop on the row Array (change row=0 if you also want to read 1st row)
+    for (var row = 1; row < rowData.length; row++) {
+
+        // Insert a row at the end of table
+        var newRow = tbodyEl.insertRow();
+
+        // Split by comma (,) to get column Array
+        rowColData = rowData[row].split(',');
+
+        // Loop on the row column Array
+        for (var col = 0; col < rowColData.length; col++) {
+
+            // Insert a cell at the end of the row
+            var newCell = newRow.insertCell();
+
+            if(col == rowColData.length-1){
+                console.log(rowColData[col]);
+                newCell.innerHTML = '<a href="'+rowColData[col]+'" target="_blank">'+rowColData[col]+'</a>';
+            } else {
+                newCell.innerHTML = rowColData[col];
+            }
+        }
+    }
 });
-
-console.log('2');
-
-/*jQuery.get('text_dumps', function(data) {
-    var myText = data;
-    console.log(data);
-    console.log('3');
-});
-
-console.log('4');
-
-var filenames=[], foldernames=[];
-
-$.get("text_dumps",function(response){
-    document.write(response);
-    getNames();
-    console.log('5');
-});
-
-function getNames()
-{
-    var files = document.querySelectorAll("a.icon.file");
-    var folders = document.querySelectorAll("a.icon.dir");
-    files.forEach(function(item){filenames.push(item.textContent)})
-    folders.forEach(function(item){foldernames.push(item.textContent.slice(0,-1))})
-    console.log(filenames);
-    console.log(foldernames);
-}*/
-
-console.log('6');
