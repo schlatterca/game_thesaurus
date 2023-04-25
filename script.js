b165002d351247f00b9b41e42484865a93df21d7
Carlo <48732275+schlatterca@users.noreply.github.com>
2023-04-26 00:15:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function sortTable(n) {
 $(document).ready(function(){
     let myImage = $('div.preview_image img')[0];
     $('#tblcsvdata > tbody > tr').mouseenter(function(){
-        let myTitle = $(this).find('td')[0].textContent.replaceAll(' ','-').replaceAll(':','').toLowerCase()+'.jpg';
+        let myTitle = $(this).find('td')[0].textContent.replaceAll(' ','-').replaceAll('é','e').replaceAll(':','').toLowerCase()+'.jpg';
         myImage.src = 'images/'+myTitle;
         $(myImage).removeClass('hidden');
     })
